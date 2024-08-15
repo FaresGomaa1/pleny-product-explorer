2c2e79cf2835de44ecf09716f7be264f00276677
FaresGomaa1 <faresgomaa336@gmail.com>
2024-08-15 19:35:11 +0300

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    PaginationComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
